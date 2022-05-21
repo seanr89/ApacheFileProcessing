6d0682dbac82479b579c4abdb566b76404829128
Sean Rafferty <seanr@randox.com>
2022-05-21 18:34:55 +0100

    First bogus transaction

---
+++ src/FakeFileCreate/Model/Transaction.cs
@@ -0,0 +1,17 @@
+
+namespace FakeFileCreate.Model;
+
+public class Transaction
+{
+    public Guid TransactionId { get; set; }
+    public Guid CustomerId { get; set; }
+    public DateTime TransactionDate { get; set; }
+    public int MCC { get; set; }
+    public int MerchantId { get; set; }
+    public decimal TransactionAmount { get; set; }
+
+    public override string ToString()
+    {
+        return $"Transaction: {TransactionId} for MCC: {MCC} with Transaction Amount: {TransactionAmount}";
+    }
+}
---
+++ src/FakeFileCreate/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+using FakeFileCreate.Model;
+
+namespace FakeFileCreate;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+
+        int recordCount = 100;
+
+        List<Transaction> records = new List<Transaction>();
+
+        for(int i = 0; i < recordCount; i++)
+        {
+            records.Add(BogusTransactionGenerator.GenerateTransaction());
+        }
+
+        Console.WriteLine(records[0].ToString());
+    }
+}
