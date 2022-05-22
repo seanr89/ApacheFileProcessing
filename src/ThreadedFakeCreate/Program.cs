b6c09a48cb6de6c7c50b9a5dedcd578606f35ebd
Sean <srafferty89@gmail.com>
2022-05-22 11:04:18 +0100

    simple work start!

---
+++ src/ThreadedFakeCreate/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace ThreadedFakeCreate;
+
+public class Program
+{
+    async static Task Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        int recordCount = 10000;
+        Console.WriteLine($"Faker Start to create: {recordCount} records");
+
+        var stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        List<Transaction> records = new List<Transaction>();
+
+        int split = 4;
+        int count = recordCount / split;
+
+        //TODO: figure out a way to split this up!
+        //Simple 4 way split now!
+        for(int i = 0; i < 4; ++i)
+        {
+            Console.WriteLine($"Iteration: {i}");
+            ThreadPool.QueueUserWorkItem(Job);
+            Thread.Sleep(200);
+        }
+
+        static void Job(object state){
+            for (int x = 0; x < 2500; x++)
+            {
+                // Console.WriteLine("cycle {0}, is processing by thread {1}",
+                // i, Thread.CurrentThread.ManagedThreadId);
+                // Thread.Sleep(350);
+                //listData.Add(BogusTransactionGenerator.GenerateTransaction());
+            }
+        }
+    }
+}
--- src/ThreadedFakeCreate/ThreadedDataGenerator.cs
+++ src/ThreadedFakeCreate/ThreadedDataGenerator.cs
@@ -1,5 +1,5 @@
 
-namespace FakeFileCreate;
+namespace ThreadedFakeCreate;
 
 public class ThreadedDataGenerator
 {
