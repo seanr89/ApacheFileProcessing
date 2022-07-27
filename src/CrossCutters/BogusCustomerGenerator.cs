ea82069bfd65523a8994a105ef6e278529dae286
Sean Rafferty <seanr@randox.com>
2022-07-27 18:12:36 +0100

    minor edits for customer

--- src/CrossCutters/BogusCustomerGenerator.cs
+++ src/CrossCutters/BogusCustomerGenerator.cs
@@ -4,7 +4,7 @@ namespace CrossCutters;
 
 public static class BogusCustomerGenerator
 {
-
+    private static string[] _genders = {"Male", "Female", "Other"};
     /// <summary>
     /// creates and returns an array of fake customers
     /// </summary>
@@ -16,7 +16,9 @@ public static class BogusCustomerGenerator
         var testTrans = new Faker<Customer>()
             //Use a method outside scope.
             .RuleFor(t => t.ExternalCustomerId, f => Guid.NewGuid())
-            .RuleFor(t => t.CustomerId, f => Guid.NewGuid());
+            .RuleFor(t => t.CustomerId, f => Guid.NewGuid())
+            .RuleFor(t => t.Gender, _genders.ElementAt(rand.Next(0, _genders.Count())))
+            .RuleFor(t => t.YoB, f => f.Random.Number(1900,2022));
 
         return testTrans.Generate(10000);
     }
--- src/CrossCutters/Models/Customer.cs
+++ src/CrossCutters/Models/Customer.cs
@@ -5,9 +5,11 @@ public record Customer
 {
     public Guid CustomerId { get; set; }
     public Guid ExternalCustomerId { get; set; }
+    public string? Gender { get; set; }
+    public int YoB { get; set; }
 
     public override string ToString()
     {
-        return $"Customer: {CustomerId} for MCC: {ExternalCustomerId}";
+        return $"Customer: {CustomerId} for MCC: {ExternalCustomerId} with Gender : {Gender} and YoB: {YoB}";
     }
 }
